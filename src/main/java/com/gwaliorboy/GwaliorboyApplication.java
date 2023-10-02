58b536c2447618a2974cf90f6f7627f73c0fe1bf
goludevops <golusingh1531@gmail.com>
2023-10-02 21:49:23 +0530

    new

---
+++ src/main/java/com/gwaliorboy/GwaliorboyApplication.java
@@ -0,0 +1,13 @@
+package com.gwaliorboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwaliorboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwaliorboyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gwaliorboy/GwaliorboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwaliorboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwaliorboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
